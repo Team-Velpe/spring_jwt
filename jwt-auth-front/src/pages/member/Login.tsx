2a9292f1fe2595623a330de139086f9efdec1109
inghyee <inghyee@gmail.com>
2022-01-18 00:12:55 +0900

    UPDATE : axios 로그인

--- jwt-auth-front/src/pages/member/Login.tsx
+++ jwt-auth-front/src/pages/member/Login.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import "../../styles/member.scss";
 
 const Login = () => {
   return (
     <div className="container">
-      <div className="form">
+      <form className="form">
         <p className="form-title">로그인 👋</p>
         <div className="form-input">
           <input type="input" placeholder="아이디" style={{ marginBottom: "0.4rem" }} />
           <input type="input" placeholder="비밀번호" />
-          <button type="submit" className="form-btn">
+          <button type="button" className="form-btn">
             로그인
           </button>
           <Link to="/join" className="form-join">
             회원가입
           </Link>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
