930b485c9b1cc28c5a1cf9b061daa58c0abcafc2
inghyee <inghyee@gmail.com>
2022-01-18 18:11:49 +0900

    UPDATE : Qna 페이지 관련 컴포넌트 분리

--- jwt-auth-front/src/pages/qna/List.tsx
+++ jwt-auth-front/src/pages/qna/List.tsx
@@ -1,58 +1,12 @@
 import React from "react";
 
+import QnaList from "../../components/QnaList";
 import "../../styles/qna.scss";
 
 const List = () => {
   return (
     <div className="qna">
-      <ul className="qna-list">
-        <li className="qna-cont">
-          <ul className="qna-title">Q.</ul>
-          <ul className="qna-info">
-            <li className="qna-question">이건 무슨 오류인가요?</li>
-            <li className="qna-date">2022-01-17</li>
-          </ul>
-        </li>
-        <li className="qna-name">
-          <p>관리자</p>님
-        </li>
-      </ul>
-      <ul className="qna-list">
-        <li className="qna-cont">
-          <ul className="qna-title">Q.</ul>
-          <ul className="qna-info">
-            <li className="qna-question">이건 무슨 오류인가요?</li>
-            <li className="qna-date">2022-01-17</li>
-          </ul>
-        </li>
-        <li className="qna-name">
-          <p>관리자</p>님
-        </li>
-      </ul>
-      <ul className="qna-list">
-        <li className="qna-cont">
-          <ul className="qna-title">Q.</ul>
-          <ul className="qna-info">
-            <li className="qna-question">이건 무슨 오류인가요?</li>
-            <li className="qna-date">2022-01-17</li>
-          </ul>
-        </li>
-        <li className="qna-name">
-          <p>관리자</p>님
-        </li>
-      </ul>
-      <ul className="qna-list">
-        <li className="qna-cont">
-          <ul className="qna-title">Q.</ul>
-          <ul className="qna-info">
-            <li className="qna-question">이건 무슨 오류인가요?</li>
-            <li className="qna-date">2022-01-17</li>
-          </ul>
-        </li>
-        <li className="qna-name">
-          <p>관리자</p>님
-        </li>
-      </ul>
+      <QnaList question="이건 무슨 오류인가요?" date="2022-01-18" nickname="관리자" />
     </div>
   );
 };
