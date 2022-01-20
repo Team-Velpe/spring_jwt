62248eb54faf7a2d4842f92608cf7468d7f8a5c4
bgchoi205 <80654620+bgchoi205@users.noreply.github.com>
2022-01-20 18:13:48 +0900

    FIX : token 검증 중복 해결

--- jwtAuth/src/main/java/com/velpe/jwtAuth/global/exception/ExceptionMessage.java
+++ jwtAuth/src/main/java/com/velpe/jwtAuth/global/exception/ExceptionMessage.java
@@ -11,7 +11,9 @@ public enum ExceptionMessage {
     DuplicatedNickname("이미 사용 중인 닉네임입니다."),
     DuplicatedEmail("이미 사용 중인 이메일입니다."),
     UsernameNotFound("존재하지 않는 회원의 아이디입니다."),
-    AccessDenied("권한이 없습니다.");
+    AccessDenied("권한이 없습니다."),
+    RefreshTokenNull("로그인 해주세요.(refreshToken null)"),
+    RefreshTokenBanned("로그인 해주세요.(refreshToken banned)");
 
     private String value;
 }
